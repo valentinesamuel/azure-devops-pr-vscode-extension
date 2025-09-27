722600f64b5ef98c77ccd326e32aaa2883e6e1eb
valentinesamuel <valentinesamuel2580@gmail.com>
2025-09-27 20:45:16 +0100

    chore: formatted with prettier

--- src/extension.ts
+++ src/extension.ts
@@ -1,42 +1,37 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from "vscode";
+import * as vscode from 'vscode';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
-  console.log(
-    'Congratulations, your extension "azure-devops-pr" is now active!',
-  );
+  console.log('Congratulations, your extension "azure-devops-pr" is now active!');
 
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  const disposable = vscode.commands.registerCommand(
-    "azure-devops-pr.helloWorld",
-    () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      vscode.window.showInformationMessage("Hello World from Azure DevOps PR!");
+  const disposable = vscode.commands.registerCommand('azure-devops-pr.helloWorld', () => {
+    // The code you place here will be executed every time your command is executed
+    // Display a message box to the user
+    vscode.window.showInformationMessage('Hello World from Azure DevOps PR!');
 
-      var panel = vscode.window.createWebviewPanel(
-        "azure-devops-pr",
-        "Azure DevOps PR",
-        vscode.ViewColumn.One,
-        { enableScripts: true },
-      );
-      panel.webview.html = getWebViewContent();
+    var panel = vscode.window.createWebviewPanel(
+      'azure-devops-pr',
+      'Azure DevOps PR',
+      vscode.ViewColumn.One,
+      { enableScripts: true },
+    );
+    panel.webview.html = getWebViewContent();
 
-      panel.webview.onDidReceiveMessage((message) => {
-        switch (message.command) {
-          case "alert":
-            vscode.window.showInformationMessage(message.text);
-        }
-      });
-    },
-  );
+    panel.webview.onDidReceiveMessage((message) => {
+      switch (message.command) {
+        case 'alert':
+          vscode.window.showInformationMessage(message.text);
+      }
+    });
+  });
 
   context.subscriptions.push(disposable);
 }
